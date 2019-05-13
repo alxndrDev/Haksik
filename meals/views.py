eba03a8b2a4083601c59ca8a34e25dd5d76bd823
alxndrDev <dev.alxndr@gmail.com>
2019-05-13 05:04:30 +0000

    changed Views.py

--- meals/views.py
+++ meals/views.py
@@ -14,10 +14,10 @@ when = ['Mon','Tue','Wed','Thu','Fri']
 
 @csrf_exempt
 def index(request):
-    
+
     return HttpResponse("Hello World. You're at Ayo")
 
-# 매주 일요일 실행 될 크롤러. 
+# 매주 일요일 실행 될 크롤러.
 #크롤링 후 데이터베이스에 저장해야한다.
 @csrf_exempt
 def crawler(request):
@@ -37,7 +37,7 @@ def crawler(request):
     for i in range(1,6):
         a = contents[i].get_text().strip('\n').split('\n')
         c.append([x for x in a if x])
-   
+
     for j in range(0,len(c)):
         for i in range(0,4):
             print(j , i)
@@ -45,16 +45,16 @@ def crawler(request):
                 c[j][i] = when[j]+"/"+str(datetime.date.today() + datetime.timedelta(days=j))
             print(c[j][i])
             print("*"*50)
-        
+
         fb = Menu(day=c[j][0], menu = c[j][1]+"/"+c[j][2]+"/"+c[j][3])
         fb.save()
-    
+
     return HttpResponse(c)
 
 
 @csrf_exempt
 def today(request):
-    
+
     none_today = "학식없음"
     morning = none_today
     lunch = none_today
@@ -62,7 +62,7 @@ def today(request):
     today_num = getWeekday()
 
     if((today_num != 5) & (today_num != 6)):
-        meal = get_Menu(1, today_num)  
+        meal = get_Menu(1, today_num)
         splited = meal.split('/')
         morning = splited[0]
         lunch = splited[1]
@@ -85,8 +85,8 @@ def tomorrow(request):
     morning = none_today
     lunch = none_today
     dinner = none_today
-    
-    if((today_num != 5) & (today_num != 6)):
+
+    if(((today_num+1) != 5) & ((today_num+1) != 6)):
         meal = get_Menu(0, today_num)
         splited = meal.split('/')
         morning = splited[0]
@@ -103,7 +103,7 @@ def tomorrow(request):
 def get_Menu(flag, today_num):
     date_str = ""
 
-    if flag == 0 :  # 내일 
+    if flag == 0 :  # 내일
         date_str = str(datetime.date.today()+datetime.timedelta(days = 1))
         meal = Menu.objects.get(day = when[today_num+1]+"/"+date_str).menu
     elif flag == 1: # 오늘
